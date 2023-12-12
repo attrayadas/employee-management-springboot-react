f0c58b220ad5d6aaa5c8f5050b7feda3534ec8aa
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-12 23:18:19 +0530

    Create EmployeeDto and EmployeeMapper

---
+++ employee-management-backend/src/main/java/com/attraya/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.attraya.ems.mapper;
+
+import com.attraya.ems.dto.EmployeeDto;
+import com.attraya.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
