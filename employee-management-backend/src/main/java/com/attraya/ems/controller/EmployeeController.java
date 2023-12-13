0b13478ef58263c4b6ccf3f2c9a06d82ae7a4f4a
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-13 15:18:25 +0530

    Build Delete Employee REST API

--- employee-management-backend/src/main/java/com/attraya/ems/controller/EmployeeController.java
+++ employee-management-backend/src/main/java/com/attraya/ems/controller/EmployeeController.java
@@ -39,4 +39,10 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeDto);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!");
+    }
+
 }
--- employee-management-backend/src/main/java/com/attraya/ems/service/EmployeeServiceImpl.java
+++ employee-management-backend/src/main/java/com/attraya/ems/service/EmployeeServiceImpl.java
@@ -48,4 +48,11 @@ public class EmployeeServiceImpl implements EmployeeService{
         Employee updatedEmployeeObj = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
     }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with given id :" + employeeId));
+        employeeRepository.deleteById(employeeId);
+    }
 }
